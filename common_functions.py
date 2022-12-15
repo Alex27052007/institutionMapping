e1581bc1a50d1a7c6d977068450788f8ed697dc3
@Alex27052007 <alexandra_wittmann@gmx.de>
2022-12-15 15:52:44 +0100

    refactor: save results function in common functions

--- common_functions.py
+++ common_functions.py
@@ -1,4 +1,5 @@
 import re
+import pandas
 
 def clean_name(row, remove, columnName):
     name_string = row[columnName]
@@ -7,3 +8,18 @@ def clean_name(row, remove, columnName):
         return cleaned_name
     else:
         return ''
+
+def save_results(links_pred, customer_nursingHomes, alberta_nursingHomes, entityName):
+    pd_result = pandas.merge(customer_nursingHomes, links_pred, how="left", left_on='Nr', right_on='Nr')
+    pd_result = pd_result.merge(alberta_nursingHomes, how='left', left_on='_id', right_index=True)
+    pd_result = pd_result.rename(columns={'Name': 'Name Kunde', 'Adresse': 'Adresse Kunde', 'PLZ': 'PLZ Kunde', 'Ort': 'Ort Kunde', 
+    'name': 'Name Alberta', 'address': 'Adresse Alberta', 'postalCode': 'PLZ Alberta', 'city': 'Ort Alberta'})
+    #del pd_result["institution_name"]
+    del pd_result["institution_address"]
+    del pd_result["name_clean"]
+    del pd_result["Name_clean"]
+    del pd_result["score"]
+    del pd_result["institution_postalcode"]
+
+    pd_result.to_csv('results_matches_' + entityName + '.csv', encoding='utf-8')
+  
